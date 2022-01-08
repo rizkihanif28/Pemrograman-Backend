f95c79758739d1a6c82a9fa9d2fdb53ff865c838
rizkihnf <rizkihnf2@gmail.com>
2022-01-08 10:10:34 +0700

    set up

---
+++ final-project/app.js
@@ -0,0 +1,10 @@
+/**
+ * TODO 1: SETUP SERVER USING EXPRESS.JS.
+ * UBAH SERVER DI BAWAH MENGGUNAKAN EXPRESS.JS.
+ * SERVER INI DIBUAT MENGGUNAKAN NODE.JS NATIVE.
+ */
+
+// membuat objek express
+const express = require("express");
+
+// menggunakan middleware
